8d95f01c4c540ab9a45c80d273b833361d260618
Jen <jnmurrin@gmail.com>
2021-05-17 17:17:05 -0500

    clean up

--- src/Body.js
+++ src/Body.js
@@ -7,14 +7,18 @@ function Body() {
   return (
     <div className="body">
       <div className="body-container">
-        <img
+        {/* <img
           className="profile-image"
           src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"
-        />
+        /> */}
 
         <div className="body__content">
           <h1>Hello, I'm Jennifer</h1>
-          <p>I like to code</p>
+          <p>
+            I'm a web developer based in the Chicago area. I have 7+ year
+            experince as a web developer in the B2B marketing space. I can help
+            your business get more leads!
+          </p>
 
           <Link to="/portfolio">
             <button className="portfolio-btn">View Portfolio</button>
